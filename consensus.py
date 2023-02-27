09e57f666d8c23800c9ffaaf67a3fcc424075599
Shorn <shrnkld@pm.me>
2023-02-27 11:21:55 +0100

    add checks for blockhash and add to consensus checks

--- consensus.py
+++ consensus.py
@@ -6,11 +6,8 @@ from collections import Counter
 from threading import Thread
 
 # Define the IP addresses to fetch the blockchain length from
-ip_addresses = ['http://130.185.119.91/', 'https://utils.blocknet.org/', 'http://api.blockinbrick.com/']
-
-
-# 'http://75.119.157.65/'
-
+ip_addresses = ['http://75.119.155.29/', 'https://utils.blocknet.org/', 'http://149.102.153.235/']
+# servers: conan, blocknet (conan), shorn
 
 def fake_data():
     global ip_addresses
@@ -22,7 +19,7 @@ def fake_data():
             'PIVX': (3726151, '6e248221277662fd0fc4758ef14dc5c86b30d5cf0687a6b544a76f20e279470a'),
             'SYS': (1556147, '481eca73fe77bd5c93a795f39d9ee6161bf7fd90d854649a992527070b5ca9e1')},
         'url_node2': {
-            'BLOCK': (2874546, 'bbb'),
+            'BLOCK': (2874546, 'aaa'),
             'DASH': (1813785, '000000000000002dadab73fb1880a606a6105156eed3451619abf820c5e34a0e'),
             'LTC': (2413782, 'd89ee7a66506f97ceca7a40b2b853ac2d7260f78cff754467219d252e89b5a65'),
             'PIVX': (3726151, '6e248221277662fd0fc4758ef14dc5c86b30d5cf0687a6b544a76f20e279470a'),
@@ -31,7 +28,7 @@ def fake_data():
             'BLOCK': (2874546, 'ccc'),
             'DASH': (1813785, '000000000000002dadab73fb1880a606a6105156eed3451619abf820c5e34a0e'),
             'LTC': (2413782, 'd89ee7a66506f97ceca7a40b2b853ac2d7260f78cff754467219d252e89b5a65'),
-            'PIVX': (3726151, '6e248221277662fd0fc4758ef14dc5c86b30d5cf0687a6b544a76f20e279470a'),
+            'PIVX': (3726151, 'sameheight_differenthash'),
             'SYS': (1556147, '481eca73fe77bd5c93a795f39d9ee6161bf7fd90d854649a992527070b5ca9e1')},
         'url_node4': {'BLOCK': (None, None), 'DASH': (None, None), 'DOGE': (None, None),
                       'LTC': (None, None), 'PIVX': (None, None), 'SYS': (None, None)}
@@ -47,9 +44,12 @@ def getblockhash(ip, coin, blockcount):
     try:
         response = requests.post(url, json=[blockcount], timeout=30)
         test = response.json()
-    except requests.exceptions.JSONDecodeError:
-        result = response.text.replace(" ", "")
-        # print("getblockhash", ip, coin, blockcount, result)
+    except requests.exceptions.JSONDecodeError as e:
+        result = None
+        print(f"Failed to parse JSON from {url}. Error: {e}")
+    except requests.exceptions.RequestException as e:
+        result = None
+        print(f"Failed to get data from {url}. Error: {e}")
     else:
         result = None
     return result
@@ -66,10 +66,6 @@ def get_heights(ip):
         blockchain_lengths[ip] = None
 
 
-# if blockchain_lengths:
-#     url = ip + "xr/" + "getblockhash"
-#     # response =  requests.get(url ,timeout=20)
-#     requests.post(url, json=[blockchain_length], timeout=60)
 
 while 1:
     ### REAL USE
@@ -107,25 +103,28 @@ while 1:
     resolved_data = {}
 
     # Iterate through all keys
+    # update for 
     snodes_count = len(ip_addresses)
     min_nodes_consensus = round(2 / 3 * snodes_count)
     print(f"total snodes_count: {snodes_count}\nmin_nodes_consensus: {min_nodes_consensus}")
     for key in all_keys:
-        values = [blockchain_lengths[ip][key] if key in blockchain_lengths[ip] else (None, None) for ip in ip_addresses
-                  if
-                  blockchain_lengths[ip] is not None]
+        values = [(blockchain_lengths[ip][key][0], blockchain_lengths[ip][key][1]) if key in blockchain_lengths[ip] else (None, None) for ip in ip_addresses
+                if
+                blockchain_lengths[ip] is not None]
         # Determine the majority value for this key
         print('key', key, 'values:', values)
         counter_obj = Counter(values)
-        most_comon = counter_obj.most_common()[0]
-        # print("most_comon:", most_comon[0], most_comon[1])
-        if (most_comon[1] >= min_nodes_consensus) & (snodes_count >= 3):
-            resolved_data[key] = most_comon[0]
+        most_common = counter_obj.most_common()[0]
+        # print("most_common:", most_common[0], most_common[1])
+        if most_common[1] >= min_nodes_consensus and snodes_count >= 3:
+            if all(value == most_common[0] for value in values if value != (None, None)):
+                resolved_data[key] = most_common[0]
+            else:
+                resolved_data[key] = (None, None)
         else:
             resolved_data[key] = (None, None)
         print(f"consensus for key {key}: {resolved_data[key]}")
 
-    print(f"Chain data: {resolved_data}")
 
     # Save data to log file
     data = {
